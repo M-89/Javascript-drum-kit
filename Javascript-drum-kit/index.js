7a8203576dd0d0ee2059a7325dfaf3734cdd72c1
Matthieu B <matthieu.baret@protonmail.com>
2021-03-16 16:15:49 +0100

    Added setStyle function and finished playSound function

--- Javascript-drum-kit/index.js
+++ Javascript-drum-kit/index.js
@@ -1,117 +1,43 @@
-"use strict";
-
-// Sélectionne toutes les divs avec classe key
+// Sélectionne toutes les divs avec la classe key
 
 const divs = document.querySelectorAll(".key");
 
-// Fonction pour jouer le son en fonction de la touche sur laquelle on appuie
+let audioFiles = document.querySelectorAll("audio");
 
-const playSound = (div, event) => {
-  // console.log(div.getAttribute("data-key"));
-
-  console.log(event);
-};
-
-// Pour chaque div dans la nodelist des divs on met un écouteur d'évènement sur le clavier
-
-divs.forEach((div, event) => {
-  div.addEventListener("keydown", playSound(div, event));
-});
+// On met un écouteur d'évènement sur la window qui écoute les touches du clavier
 
 window.addEventListener("keydown", event => {
-  console.log(event);
+  playSound(event);
 });
 
-// Sélectionner les divs des touches et crée un tableau avec
+// Fonction pour jouer le son en fonction de la touche sur laquelle on appuie
 
-const keys = document.querySelectorAll(".key");
+const playSound = event => {
+  // On itère sur les divs
+  divs.forEach(div => {
+    if (div.dataset.key == event.keyCode) {
+      // Si on trouve une div qui a le même dataset.key que le numéro de la touche
+      audioFiles.forEach(audioFile => {
+        // On itère sur les fichiers audio
+        if (audioFile.dataset.key == event.keyCode) {
+          // Si on trouve un fichier audio qui a le même dataset.key que le numéro de la touche on joue le fichier audio correspondant, et on lance la fonction pour changer le style sur la div correspondante
+          audioFile.currentTime = 0; // Rembobine le son
+          audioFile.play();
+          setStyle(div);
+        }
+      });
+    }
+  });
+};
 
-// Faire jouer les sons en appuyant sur les touches, et mettre l'animation sur la bonne div en fonction de celle sur laquelle on appuie. Stopper l'animation au bout de 50 ms.
+const setStyle = div => {
+  div.classList.add("playing");
 
-/* window.addEventListener("keydown", e => {
-  switch (e.keyCode) {
-    case 65:
-      audioKick.currentTime = 0;
-      audioKick.load();
-      audioKick.play();
-      keys[0].classList.add("playing");
-      setTimeout(() => {
-        keys[0].classList.remove("playing");
-      }, 50);
-      break;
-    case 83:
-      audioSnare.currentTime = 0;
-      audioSnare.load();
-      audioSnare.play();
-      keys[1].classList.add("playing");
-      setTimeout(() => {
-        keys[1].classList.remove("playing");
-      }, 50);
-      break;
-    case 68:
-      audioCh.currentTime = 0;
-      audioCh.load();
-      audioCh.play();
-      keys[2].classList.add("playing");
-      setTimeout(() => {
-        keys[2].classList.remove("playing");
-      }, 50);
-      break;
-    case 70:
-      audioMh.currentTime = 0;
-      audioMh.load();
-      audioMh.play();
-      keys[3].classList.add("playing");
-      setTimeout(() => {
-        keys[3].classList.remove("playing");
-      }, 50);
-      break;
-    case 71:
-      audioOh.currentTime = 0;
-      audioOh.load();
-      audioOh.play();
-      keys[4].classList.add("playing");
-      setTimeout(() => {
-        keys[4].classList.remove("playing");
-      }, 50);
-      break;
-    case 72:
-      audioClap.currentTime = 0;
-      audioClap.load();
-      audioClap.play();
-      keys[5].classList.add("playing");
-      setTimeout(() => {
-        keys[5].classList.remove("playing");
-      }, 50);
-      break;
-    case 74:
-      audioHighTom.currentTime = 0;
-      audioHighTom.load();
-      audioHighTom.play();
-      keys[6].classList.add("playing");
-      setTimeout(() => {
-        keys[6].classList.remove("playing");
-      }, 50);
-      break;
-    case 75:
-      audioMidTom.currentTime = 0;
-      audioMidTom.load();
-      audioMidTom.play();
-      keys[7].classList.add("playing");
-      setTimeout(() => {
-        keys[7].classList.remove("playing");
-      }, 50);
-      break;
-    case 76:
-      audioLowTom.currentTime = 0;
-      audioLowTom.load();
-      audioLowTom.play();
-      keys[8].classList.add("playing");
-      setTimeout(() => {
-        keys[8].classList.remove("playing");
-      }, 50);
-      break;
-  }
-}); */
-
-// KeyboardEvent.keyCode est déprécié, mieux vaut utiliser KeyboardEvent.code. J'ai utilisé KeyboardEvent.keyCode car les keycodes étaient mappés en fonction de cet event dans le HTML. Les mappings (les numéros / noms des touches) sont différents avec KeyboardEvent.code donc je n'ai pas voulu tout changer dans le HTML.
+  // On écoute l'évènement "transitionend" sur la div
+  div.addEventListener("transitionend", e => {
+    // Si l'évènement "transitionend" est appelé sur la propriété transform on enlève la classe "playing"
+    if (e.propertyName === "transform") {
+      div.classList.remove("playing");
+    }
+  });
+};
